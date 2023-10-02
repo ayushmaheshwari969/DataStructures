ac17f3fa3d5055cbf4e1644c76d35d2b4e1fdbde
ayushmaheshwari969 <97792343+ayushmaheshwari969@users.noreply.github.com>
2023-10-02 23:49:16 +0530

    InsertionSinglyLinkedList.cpp
    Insertion Operation in Singly linked list , at head , at tail and in between

--- SinglyLinkedList.cpp
+++ SinglyLinkedList.cpp
@@ -1,160 +1,81 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-
-class node
-{
-
-public:
-  int data;
-
-  node *next;
-
-
-node (int data)
-  {
-    this->data = data;
-    this->next = NULL;
-  }
+class node{
+  public:
+        int data;
+        node* next;
+        node(int d)
+        {
+            this->data = d;
+            this->next = NULL;
+        }
 };
 
-
-
-void
-InsertatHead (node * &head, int d)
+//Insert at First position of list 
+void InsertatHead(node * &head , int d)
 {
-
-// New node
-  node *temp = new node (d);
-
-  temp->next = head;
-
-  head = temp;
-
+    node* temp = new node(d);
+    temp ->next = head;
+    head = temp;
 }
 
-void
-InsertatTail (node * &tail, int d)
+//Insert at Last position of list
+void InsertatTail(node*&tail , int d)
 {
-
-//new node
-  node *temp = new node (d);
-
-  tail->next = temp;
-
-  tail = temp;
-
+    node* temp = new node(d);
+    tail->next = temp;
+    tail = temp;
 }
 
-void
-InsertatPosition (node * &head, node * &tail, int position, int d)
+//Insert at any position
+void InsertatPosition(node* &head , node* &tail , int position , int d)
 {
-
-  if (position == 1)		// Insertion at head
+    if(position == 1)
     {
-
-      InsertatHead (head, d);
-
-      return;
-
+        InsertatHead(head , d);
+        return;
     }
-
-  node *temp = head;
-
-  int cnt = 1;
-
-  while (cnt < position - 1)
-
+    
+    int cnt = 1;
+    node* temp = head;
+    while(cnt < position - 1)
     {
-
-      temp = temp->next;
-
-      cnt++;
-
+        temp = temp -> next;
+        cnt++;
     }
-
-  if (temp->next == NULL)	// Last position Insertion at tail
+    
+    if(temp -> next == NULL)
     {
-
-      InsertatTail (tail, d);
-
-      return;
-
+        InsertatTail(tail , d);
+        return;
     }
-
-
-  node *toinsert = new node (d);
-
-  toinsert->next = temp->next;
-
-  temp->next = toinsert;
-
+    
+    node* toinsert = new node(d);
+    toinsert -> next = temp-> next;
+    temp -> next = toinsert;
+    
 }
-
-
-
-void
-printHead (node * &head)
+//Print the list
+void printlist(node* &head)
 {
-
-  node *temp = head;
-
-  while (temp != NULL)
-
+    node* temp = head;
+    while(temp != NULL)
     {
-
-      cout << temp->data << " ";
-
-      temp = temp->next;
-
+        cout<<temp->data<<',';
+        temp = temp->next;
     }
-
-  cout << endl;
-
 }
-
-
-int
-main ()
+int main()
 {
-
-  node *node1 = new node (10);
-
-  node *head = node1;
-
-  node *tail = node1;
-
-/* printHead(head);
-InsertatHead(head , 12);
-printHead(head);
-InsertatHead(head , 15);
-printHead(head);*/
-//printHead(node1);
-  InsertatTail (tail, 12);
-
-//printHead(node1);
-  InsertatTail (tail, 15);
-
-  printHead (node1);
-
-
-  InsertatPosition (node1, tail, 4, 30);
-
-  printHead (head);
-
-  cout << head->data << endl;
-
-  cout << tail->data << endl;
-
-  InsertatPosition (head, tail, 1, 50);
-
-  printHead (head);
-
-  cout << head->data << endl;
-
-  cout << tail->data << endl;
-
-
-  return 0;
-
+    node* node1 = new node(10);
+    node* head = node1;
+    node* tail = node1;
+    
+    InsertatHead(head , 14);
+    InsertatTail(tail , 15);
+    InsertatTail(tail , 16);
+    InsertatHead(head , 12);
+    InsertatPosition(head , tail , 4 , 9);
+    printlist(head);
 }
